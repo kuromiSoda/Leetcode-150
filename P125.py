742417b0005c9b0a6c1e15634cc7e89a52728b73
Anushka Siri Raghunandan <ansira7@outlook.com>
2025-04-05 20:33:21 +0530

    Update P125.py

--- P125.py
+++ P125.py
@@ -4,3 +4,15 @@ class Solution:
         if s==s[::-1]:
             return True
         return False
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=[x.lower() for x in s if x.isalnum()]
+        l,r=0,len(s)-1
+        while l<=r:
+            if s[l]==s[r]:
+                l+=1
+                r-=1
+            else:
+                return False
+        return True
